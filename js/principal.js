c4480abb9a97e6dfd60cd667d01121de3b737367
Marcos Paulo M Cabral <paulo_kpv@hotmail.com>
2021-04-14 23:35:37 -0300

    .

--- js/principal.js
+++ js/principal.js
@@ -3,12 +3,23 @@ function iniciar() {
     
     while (personagem != 1 || personagem != 2 || personagem != 3) {
         var personagem = prompt("Escolha Seu Personagem" + "\n" + "Selecione 1 para  escolher Abgail \nSelecione 2 para escolher electra \nSelecione 3  para escolher Zion")
-        if (personagem == 1 || personagem == 2 || personagem == 3) {
+        if (personagem == 1) {
+            alert("Você iniciará a Jornada de Abgail")
+            location.assign('cap1.html')
+            break
+        }
+        else if(personagem==2){
+            alert("Você iniciará a Jornada de Electra ")
+            location.assign('cap1.html')
+            break
+        }
+        else if(personagem==3){
+            alert("Você iniciará a Jornada de Zion")
             location.assign('cap1.html')
             break
         }
         else{
-            alert("entre com um comando válido")
+        alert("entre com um comando válido")
         }
     }
 }
